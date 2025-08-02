edafdfef750b6011005ed3236c7c45f2bc040310
visagan1234-ops <visagan6381049829@gmail.com>
2025-08-02 22:39:27 +0530

    fix images

--- data/index.ts
+++ data/index.ts
@@ -12,7 +12,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/portfolio/b1.svg",
     spareImg: "",
   },
   {
@@ -42,8 +42,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: "/portfolio/grid.svg",
+    spareImg: "/portfolio/b4.svg",
   },
 
   {
@@ -53,8 +53,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: "/portfolio/b5.svg",
+    spareImg: "/portfolio/grid.svg",
   },
   {
     id: 6,
@@ -73,32 +73,32 @@ export const projects = [
     id: 1,
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    img: "/portfolio/p1.svg",
+    iconLists: ["/portfolio/re.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/fm.svg"],
+    link: "ui.earth.com",
   },
   {
     id: 2,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    img: "/portfolio/p2.svg",
+    iconLists: ["/portfolio/next.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/stream.svg", "/portfolio/c.svg"],
+    link: "ui.yoom.com",
   },
   {
     id: 3,
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
+    img: "/portfolio/p3.svg",
+    iconLists: ["/portfolio/re.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/c.svg"],
+    link: "ui.aiimg.com",
   },
   {
     id: 4,
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    img: "/portfolio/p4.svg",
+    iconLists: ["/portfolio/next.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/gsap.svg"],
     link: "/ui.apple.com",
   },
 ];
@@ -106,17 +106,17 @@ export const projects = [
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "/portfolio/git.svg",
     href:"https://github.com/visagan1234-ops"
   },
   {
     id: 2,
-    img: "/twit.svg",
+    img: "/portfolio/twit.svg",
     href:""
   },
   {
     id: 3,
-    img: "/link.svg",
+    img: "/portfolio/link.svg",
     href:"https://linkedin.com/in/visagan-r-1012772a7"
   },
 ];
